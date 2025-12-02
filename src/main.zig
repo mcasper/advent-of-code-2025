e139148199585fc8e10d5ce4e1c0410e6101996d
Matt Casper <matthewvcasper@gmail.com>
2025-12-01 17:30:20 -0800

    Day 1, Part 2

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const answer = try part1();
-    std.debug.print("Day 1 Part 1 answer: {d}\n", .{answer});
+    const part1_answer = try part1();
+    std.debug.print("Day 1 Part 1 answer: {d}\n", .{part1_answer});
+
+    const part2_answer = try part2();
+    std.debug.print("Day 1 Part 2 answer: {d}\n", .{part2_answer});
 }
 
 fn part1() !u64 {
@@ -32,6 +35,63 @@ fn part1() !u64 {
                     current_position += distance;
                     while (current_position > 99) {
                         current_position -= 100;
+                    }
+                },
+                else => {
+                    std.debug.print("what??\n", .{});
+                },
+            }
+
+            if (current_position == 0) {
+                num_times_zero += 1;
+            }
+        }
+    }
+
+    return num_times_zero;
+}
+
+fn part2() !u64 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const input_buffer = try readFilePath(allocator, "data/day1/input.txt");
+    defer allocator.free(input_buffer);
+
+    var current_position: i64 = 50;
+    var num_times_zero: u64 = 0;
+
+    var line_it = std.mem.splitScalar(u8, input_buffer, '\n');
+    while (line_it.next()) |line| {
+        if (line.len > 0) {
+            switch (line[0]) {
+                'L' => {
+                    const distance = try std.fmt.parseInt(u32, line[1..], 10);
+                    const laps = distance / 100;
+                    const before_position = current_position;
+                    current_position -= @rem(distance, 100);
+                    num_times_zero += laps;
+
+                    if (current_position < 0) {
+                        current_position += 100;
+                        if (before_position != 0 and current_position != 0) {
+                            num_times_zero += 1;
+                        }
+                    }
+                },
+                'R' => {
+                    const distance = try std.fmt.parseInt(u32, line[1..], 10);
+                    const laps = distance / 100;
+                    const before_position = current_position;
+                    current_position += @rem(distance, 100);
+                    num_times_zero += laps;
+
+                    if (current_position > 99) {
+                        current_position -= 100;
+                        if (before_position != 0 and current_position != 0) {
+                            num_times_zero += 1;
+                        }
                     }
                 },
                 else => {
